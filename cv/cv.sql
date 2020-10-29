ae6cb72039bde8124980bf88bbb4286aac514a3a
ibtissembdh <ibtissem.bendouhou754@gmail.com>
2020-10-29 23:30:18 +0100

    update database file

--- cv/cv.sql
+++ cv/cv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 28 sep. 2020 à 20:28
+-- Généré le : jeu. 29 oct. 2020 à 23:26
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.2.28
 
@@ -39,11 +39,8 @@ CREATE TABLE `competence` (
 --
 
 INSERT INTO `competence` (`idCom`, `user_id`, `competence`) VALUES
-(103, 75, 'xxxxxxxx'),
 (110, 82, 'xxxxxxxx'),
-(111, 83, 'xxxxxxxx'),
-(113, 144, 'java'),
-(114, 144, 'php');
+(117, 143, 'php');
 
 -- --------------------------------------------------------
 
@@ -65,13 +62,9 @@ CREATE TABLE `experience` (
 --
 
 INSERT INTO `experience` (`idEX`, `user_id`, `intituleE`, `ex_dateDebut`, `ex_dateFin`, `ex_description`) VALUES
-(97, 75, 'dddddd', '2222-02-22', '2222-02-22', 'xxxxxxxxx'),
-(104, 82, 'dddddd', '2020-06-20', '2020-06-20', 'vuggyg'),
-(105, 83, 'infographie', '2222-02-22', '2222-02-22', 'vuggyg'),
+(104, 82, 'two year dev web', '2020-06-20', '2020-06-20', 'radome text '),
 (126, 143, 'infographie 2D', '2222-02-22', '2222-02-22', 'desc infographie 2D'),
-(127, 143, 'web', '2020-04-03', '2022-04-05', 'desc web'),
-(128, 144, 'ex un', '2020-07-01', '2020-09-06', 'ex desc de un'),
-(129, 144, 'ex deux', '2020-06-03', '2020-06-13', 'desc de deux');
+(127, 143, 'web', '2020-04-03', '2022-04-05', 'desc web');
 
 -- --------------------------------------------------------
 
@@ -93,13 +86,9 @@ CREATE TABLE `formation` (
 --
 
 INSERT INTO `formation` (`idFR`, `user_id`, `intituleF`, `dp_dateDebut`, `dp_dateFin`, `dp_description`) VALUES
-(106, 75, 'mohim formation', '2222-02-22', '2222-02-22', 'mohom description'),
-(113, 82, 'soft developer', '2020-06-20', '2020-06-20', 'jnjknjknjknjk'),
-(114, 83, 'ssssssss', '2222-02-22', '2222-02-22', 'jnjknjknjknjk'),
+(113, 82, 'computer science', '2020-06-20', '2020-06-20', 'random text'),
 (135, 143, 'devlopememt web et infographie', '2222-02-22', '2222-02-22', 'desc dev web and info'),
-(136, 143, 'front web dev', '2222-02-22', '2222-02-22', 'desc front web '),
-(137, 144, 'de un', '2020-09-03', '2020-09-06', 'dec de un'),
-(138, 144, 'de deux', '2020-10-04', '2020-10-25', 'decv de deux');
+(136, 143, 'front web dev', '2222-02-22', '2222-02-22', 'desc front web ');
 
 -- --------------------------------------------------------
 
@@ -118,13 +107,9 @@ CREATE TABLE `loisir` (
 --
 
 INSERT INTO `loisir` (`idLoi`, `user_id`, `loisir`) VALUES
-(97, 75, 'zzzzzzzz'),
 (104, 82, 'zzzzzzzz'),
-(105, 83, 'hhj'),
 (126, 143, 'sport'),
-(127, 143, 'swim'),
-(128, 144, 'besket'),
-(129, 144, 'foot');
+(127, 143, 'swim');
 
 -- --------------------------------------------------------
 
@@ -148,11 +133,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `tel`, `email`, `titre`, `date`, `photo`) VALUES
-(75, 'adel', 'ibtissem', 33333333, '0660481370', 'zzzzzz', '2222-02-22', 0x433a5c66616b65706174685c6c6f676f522d46494e414c452e706e67),
-(82, 'zzzzz', 'ibtissem', 33333333, 'adel.chiekh754@gmail.com', 'zzzzzz', '2020-06-20', 0x433a5c66616b65706174685c6c6f676f522d46494e414c452e706e67),
-(83, 'ben', 'ibtissem', 33333333, 'Sofianebendouhou95@gmail.com', 'developpement  web', '2222-02-22', 0x433a5c66616b65706174685c494d475f393932352e4a5047),
-(143, 'yasmina', 'bendouhou', 2147483647, 'yasmine@gmail.com', 'web developper', '2000-03-12', ''),
-(144, 'bendouhouu', 'saliima', 214748364, 'salima@gmail.com', 'banqua', '2010-09-27', '');
+(82, 'bndh', 'ibtissem', 33333333, 'ibtissem@gmail.com', 'full stuck web dev', '2020-06-20', 0x433a5c66616b65706174685c6c6f676f522d46494e414c452e706e67),
+(143, 'john', 'Doe', 2147483647, 'john@gmail.com', 'web developper', '2000-03-12', '');
 
 --
 -- Index pour les tables déchargées
@@ -201,31 +183,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `competence`
 --
 ALTER TABLE `competence`
-  MODIFY `idCom` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
+  MODIFY `idCom` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT pour la table `experience`
 --
 ALTER TABLE `experience`
-  MODIFY `idEX` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `idEX` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131;
 
 --
 -- AUTO_INCREMENT pour la table `formation`
 --
 ALTER TABLE `formation`
-  MODIFY `idFR` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
+  MODIFY `idFR` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 
 --
 -- AUTO_INCREMENT pour la table `loisir`
 --
 ALTER TABLE `loisir`
-  MODIFY `idLoi` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `idLoi` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
 
 --
 -- Contraintes pour les tables déchargées
